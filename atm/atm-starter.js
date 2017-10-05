c35ce9a62916f1d3f23fb4d98882b0d9da271d04
Leon Ormes <leonormes@gmail.com>
2017-10-05 12:43:39 +0100

    Set up user collection

--- atm/atm-starter.js
+++ atm/atm-starter.js
@@ -1,5 +1,25 @@
 var R = require('ramda'); // Helps with {}s and []s http://ramdajs.com/docs/#find
 var prompt = require('prompt');
+
+var users = [
+  {
+    name: 'Leon',
+    card_number: 01234567,
+    pin: 3324,
+    balance: 1433,
+    accLocked: false,
+    authenticated: false
+  },
+  {
+    name: 'Toby',
+    card_number: 88875654,
+    pin: 1232,
+    balance: 133,
+    accLocked: false,
+    authenticated: false
+  }
+]
+
 prompt.start(); // Lets you prompt the user for info
 
 // Dummy Users
