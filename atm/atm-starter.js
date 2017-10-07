6f3916b5cf42af77cf6a8b3a698c9119678a8cd8
Leon Ormes <leonormes@gmail.com>
2017-10-07 16:32:01 +0100

    users object

--- atm/atm-starter.js
+++ atm/atm-starter.js
@@ -1,58 +1,47 @@
 var R = require('ramda'); // Helps with {}s and []s http://ramdajs.com/docs/#find
 var prompt = require('prompt');
+const users = require('./usersObject');
 
-var users = [{
-    name: 'Leon',
-    card_number: 51234567,
-    pin: 3324,
-    balance: 1433,
-    accLocked: false,
-    authenticated: false
-  },
-  {
-    name: 'Toby',
-    card_number: 88875654,
-    pin: 1232,
-    balance: 133,
-    accLocked: false,
-    authenticated: false
-  }
-];
-const schema = {
-  properties: {
-    cardNumber: {
-      description: 'Please enter your 8 digit card number:',
-      required: true,
-      pattern: /^(\d{8})$/,
-      message: '8 digits only'
-    },
-    pin: {
-      description: 'Please enter your 4 digit pin',
-      pattern: /^(\d{4})$/,
-      message: '4 digit pin only',
-      hidden: true,
-      replace: '*',
-      required: true
-    },
-  },
-};
-init();
-function init() {
-  prompt.start();
-  prompt.get(schema, function (err, result) {
-    if (err) { // Handle error
-      return err;
+const card = {
+    properties: {
+        cardNumber: {
+            description: 'Please enter your 8 digit card number:',
+            required: true,
+            pattern: /^(\d{8})$/,
+            message: '8 digits only'
+        }
     }
-    validateCard(result);
-  });
+}
+const pin = {
+    properties: {
+        pin: {
+            description: 'Please enter your 4 digit pin',
+            pattern: /^(\d{4})$/,
+            message: '4 digit pin only',
+            hidden: true,
+            replace: '*',
+            required: true
+        },
+    }
+}
+init();
+
+function init() {
+    prompt.start();
+    prompt.get(card, function (err, result) {
+        if (err) { // Handle error
+            return err;
+        }
+        return parseInt(result);
+    }).then(validatePin());
 };
 
 function validateCard(result) {
-  let cn = R.find(R.propEq('card_number', parseInt(result.cardNumber)))(users);
-  if(cn === undefined) {
-    console.log('Unknown card');
-    init();
-  } else {
-    validatePin();
-  }
+    let cn = R.find(R.propEq('card_number', parseInt(result.cardNumber)))(users);
+    if (cn === undefined) {
+        console.log('Unknown card');
+        init();
+    } else {
+        return;
+    }
 }
---
+++ atm/usersObject.js
@@ -0,0 +1,20 @@
+
+const users = [{
+        name: 'Leon',
+        card_number: 51234567,
+        pin: 3324,
+        balance: 1433,
+        accLocked: false,
+        authenticated: false
+    },
+    {
+        name: 'Toby',
+        card_number: 88875654,
+        pin: 1232,
+        balance: 133,
+        accLocked: false,
+        authenticated: false
+    }
+];
+
+module.exports = users;
