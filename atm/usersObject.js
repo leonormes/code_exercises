0e1ab8692e6282225378cf1c6f97ce3b61da715e
Leon Ormes <leonormes@gmail.com>
2017-10-07 19:42:04 +0100

    Add a few users

--- atm/usersObject.js
+++ atm/usersObject.js
@@ -1,9 +1,26 @@
 
-const users = [{
+const users = [
+    {
         name: 'Leon',
         cardNumber: 51234567,
         pin: 3324,
         balance: 1433,
+        accLocked: false,
+        authenticated: false
+    },
+    {
+        name: 'Ben',
+        cardNumber: 76543217,
+        pin: 1234,
+        balance: 31433,
+        accLocked: false,
+        authenticated: false
+    },
+    {
+        name: 'Clara',
+        cardNumber: 35465768,
+        pin: 3344,
+        balance: 3465,
         accLocked: false,
         authenticated: false
     },
