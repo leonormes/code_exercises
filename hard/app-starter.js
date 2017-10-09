656983a4948bcb03128dd02e913aefea5b22a034
Leon Ormes <leonormes@gmail.com>
2017-10-09 15:38:43 +0100

    Start

--- hard/app-starter.js
+++ hard/app-starter.js
@@ -1,4 +1,17 @@
 var prompt = require('prompt');
+prompt.message = '?';
+var schema ={
+  properties: {
+    year: {
+      description: 'Year of Birth (YYYY)',
+      pattern: /^\d{4}$/,
+      message: 'needs to be of the format YYYY',
+      required: true
+    }
+  }
+}
 prompt.start(); // Lets you prompt the user for info
 
-var yearRegex = /^\d{4}$/;
+prompt.get(schema, function(err, result) {
+  console.log(result)
+})
--- psn/app-starter.js
+++ psn/app-starter.js
@@ -1,14 +1,25 @@
 var prompt = require('prompt');
+prompt.message = '?';
+prompt.colors = false;
 prompt.start(); // Lets you prompt the user for info
 
 var _pet_name = null;
 var _mothersMaidenName = null;
 
-console.log('What is the name of your first pet??');
 // Prompt (which is Async) works like this:
-prompt.get(['pet_name'], function (err, result) {
+prompt.get([{
+  name: 'pet_name',
+  description: 'your first pets name',
+  type: 'string'},
+{
+  name: 'maidenName',
+  description: 'Mothers maiden name',
+  type: 'string'
+}], function (err, result) {
   if (err) { // Handle error
     return err;
   }
+  console.log('Your porn star name is: ' + result.pet_name + ' '
+   + result.maidenName)
 
 });
